5369759539a2a1734c70afadb33a65823f17fc1b
JustinBenito <justinbenito1974@gmail.com>
2023-10-20 20:27:56 +0530

    done done

--- src/components/aichat.jsx
+++ src/components/aichat.jsx
@@ -1,135 +1,168 @@
-import React, { useState } from 'react';
-import TokenSwap from './transactioncard';
+import React, { useState, useEffect } from 'react';
 import Transak from './transak';
-import OpenAI from "openai";
+import OpenAI from 'openai';
 
 const AIChat = () => {
   const [userMessage, setUserMessage] = useState('');
   const [messages, setMessages] = useState([]);
-
-  const [response, setResponse]=useState([]);  
-
-  const [chatmessage, setChatMessage]=useState([])
-  const [functionmessage, setFunctionMessage]=useState([])
+  const [response, setResponse] = useState(null);
+  const [chatmessage, setChatMessage] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleUserMessageChange = (e) => {
     setUserMessage(e.target.value);
   };
 
   const openai = new OpenAI({
-    apiKey: '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',
+    apiKey: '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',
     dangerouslyAllowBrowser: true,
     maxRetries: 0,
   });
 
-  const fetch_data = async () => {
-    const response = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo-0613',
-        messages: chatmessage,
-        functions: functionmessage,
-        function_call: 'auto',
-      });
-      console.log(chatmessage, functionmessage)
-      console.log(response)
-      setResponse(response)
-  }
+  const functions1=[
+    {
+        name: "transak",
+        description: `BUY or SELL Cryptocurrency `,
+        parameters: {
+          type: "object",
+          properties: {
+            command: {
+              type: "string",
+              description: `BUY or SELL, default is BUY`,
+            },
+            fiatamount: { 
+              type: "number", 
+              description: `value of fiatcurrency eg. 1000, usd etc. 0 if the command is SELL`
+            },
+            paymentmethod:{
+
+                type: "string",
+                description: `choose the relevant option, 
+                eg. 
+                credit_debit_card
+                apple_pay
+                google_pay
+                sepa_bank_transfer
+                gbp_bank_transfer `
+            },
+            fiatcurrency:{
+                type: "string",
+                description: "what is the currency, eg. INR, USD, GBP",
+            },
+            countrycode:{
+                type: "string",
+                description: "what is the countrycode for the currency eg. IN,US"
+            },
+            network: { 
+              type: "string", 
+              description: `What is the network that the crypto is in eg. Ethereum`
+            },
+            cryptoamount:{
+                type: "number",
+                description: "if its SELL, how much to SELL",
+            },
+            cryptocurrency: {type: "string",
+            description: "if its SELL, what to SELL"
+            },
+            cryptocurrencycode: {
+                type: "string",
+                description: "code of the cryptocurrency"
+            },
+            email:{
+                type: "string",
+                description: "email of the user"
+            },
+            walletaddress: {
+                type: "string",
+                description: "wallet address of user"
+            },
+            hideexchange: {
+                type: "boolean",
+                description: "hide exchange true or false"
+            },
+            walletform: {
+                type: "boolean",
+                description: "Disable Walletform true or false"
+            }
+          },
+          required: ["command",'fiatamount','paymentmethod','fiatcurrency','countrycode','network','cryptoamount','cryptocurrency','cryptocurrencycode','email','walletaddress','walletform'],
+        },
+      },
+]
 
   const handleSendMessage = () => {
     if (userMessage) {
-      setMessages([...messages, userMessage]);
-      setUserMessage('');
+      const message1 = [
+        { role: 'system', content: 'Only use the function I provide' },
+        { role: 'user', content: `${userMessage}` },
+      ];
+
+      setChatMessage(message1);
+      console.log(chatmessage)
     }
 
-    if(userMessage){
-        const message1=[
-            {role:'system',content: "Only use the function I provide"},
-            {role: "user", content: `${userMessage}` },
-        ]
-        setChatMessage(message1)
-
-        const functions1=[
-            {
-                name: "transak",
-                description: `BUY or SELL Cryptocurrency `,
-                parameters: {
-                  type: "object",
-                  properties: {
-                    command: {
-                      type: "string",
-                      description: `BUY or SELL`,
-                    },
-                    fiatamount: { 
-                      type: "number", 
-                      description: `value of fiatcurrency i.e rupees, usd etc. 0 if the command is SELL`
-                    },
-                    fiatcurrency:{
-                        type: "string",
-                        description: "what is the currency, eg. INR, USD, GBP",
-                    },
-                    countrycode:{
-                        type: "string",
-                        description: "what is the countrycode for the currency eg. IN,US"
-                    },
-                    network: { 
-                      type: "string", 
-                      description: `What is the network that the crypto is in eg. Ethereum`
-                    },
-                    cryptoamount:{
-                        type: "number",
-                        description: "if its SELL, how much to SELL",
-                    },
-                    cryptocurrency: {type: "string",
-                    description: "if its SELL, what to SELL"
-                    },
-                    cryptocurrencycode: {
-                        type: "string",
-                        description: "code of the cryptocurrency"
-                    }
-                  },
-                  required: ["command",'fiatamount','fiatcurrency','countrycode','network','cryptocurrency','cryptocurrencycode'],
-                },
-              },
-        ]
-        
-        setFunctionMessage(functions1)
-
-        fetch_data()
-    }
   };
 
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      handleSendMessage();
+  useEffect(() => {
+    if (chatmessage.length > 0) {
+      setLoading(true);
+      fetch_data()
+        .then((response) => {
+          setResponse(response);
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, [chatmessage]);
+
+  const fetch_data = async () => {
+    console.log("im in")
+    try {
+      const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo-0613',
+        messages: chatmessage,
+        functions: functions1,
+        function_call: 'auto',
+      });
+
+      console.log(response)
+      setLoading(false)
+      return response;
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
   };
 
   return (
-    <div className="h-screen bg-gradient-to-r from-purple-500 to-blue-500 flex flex-col">
-      <div className="flex-grow p-8 overflow-y-auto">
-        {/* Placeholder content frame */}
-        <div className="bg-white rounded-lg p-4 shadow-lg">
-          <h1 className="text-2xl font-semibold mb-4">Web3 UI</h1>
-          <div className="text-gray-600">
-            {messages.map((message, index) => (
-              <p key={index}>{message}</p>
-            ))}
+    <div className="h-screen w-full bg-gradient-to-r from-purple-500 to-blue-500 flex flex-col gap-8">
+      <div className="h-full rounded-2xl flex justify-center">
+        {loading ? (
+          <div className="text-white">Loading...</div>
+        ) : response && response.model === 'gpt-3.5-turbo-0613' ? (
+          <div className='p-18 w-full rounded-xl'>
+            <Transak resp={response} />
           </div>
-          <Transak resp={response}/>
-          {/* Add more components here */}
-        </div>
+        ) : (
+          <div className='text-white'></div>
+        )}
       </div>
+
       <div className="p-4">
         <input
           type="text"
           placeholder="Type your message..."
           value={userMessage}
           onChange={handleUserMessageChange}
-          onKeyPress={handleKeyPress}
           className="w-full py-2 px-3 rounded-lg shadow focus:outline-none focus:ring focus:border-blue-300"
         />
         <button
           onClick={handleSendMessage}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-lg w-full mt-2 focus:outline-none"
+          className="bg-blue-500 hover-bg-blue-600 text-white font-medium py-2 px-4 rounded-lg w-full mt-2 focus:outline-none"
         >
           Send
         </button>
--- src/components/transak.jsx
+++ src/components/transak.jsx
@@ -1,34 +1,68 @@
+
+
 import React from 'react'
-// import transakSDK from '@transak/transak-sdk';
+
 
  const Transak = (resp) => {
     console.log("this is in Transak", resp)
-    const response_message = resp.choices[0].message.function_call.arguments;
-    console.log(response_message)
-    const APIKEY="ef4f99f1-99f5-4897-9a1d-983d7371eaf3"
-    // const args="{
-    //     "command": "SELL",
-    //     "fiatamount": 0,
-    //     "fiatcurrency": "",
-    //     "countrycode": "",
-    //     "network": "",
-    //     "cryptoamount": 30,
-    //     "cryptocurrency": "ETH",
-    //     "cryptocurrencycode": ""
-    //   }"
+    const responseis = resp.resp
+
+    const finalresp = JSON.parse(responseis.choices[0].message.function_call.arguments)
+
+    const APIKEY="c6016886-3759-4f3f-bba0-8602cf4a6401"
+
+
+    console.log(
+        `
+        <iframe
+        id="transakIframe"
+        src={https://global-stg.transak.com/?apiKey=${APIKEY}&environment=STAGING&widgetHeight=100%&widgetWidth=100%&exchangeScreenTitle=Buy%20Crypto&productsAvailed=${finalresp.command}&fiatAmount=${finalresp.fiatamount}&paymentMethod=${finalresp.paymentmethod || "credit_debit_card"}&fiatCurrency=${finalresp.fiatcurrency}&countryCode=${finalresp.countrycode}&network=${finalresp.network}&cryptoCurrencyCode=${finalresp.cryptocurrencycode}&email=${finalresp.email}&themeColor=#ffc0cb}
+        allow="camera;microphone;payment"
+        style="height: 100%; width: 100%; border: none">
+    </iframe> 
+    `
+    )
 
 
 
   return (
-   <div style="position: relative; width: 500px; height: 80dvh; margin: auto; box-shadow: 0 0 15px #1461db; border-radius: 15px; overflow: hidden">
-            <iframe
+<div className='h-full'>
+            {finalresp.command=="BUY" ?
+
+                <iframe
                 id="transakIframe"
-                src={`https://global-stg.transak.com/?apiKey=${APIKEY}&environment=STAGING&widgetHeight=100%&widgetWidth=100%&exchangeScreenTitle=Buy%20Crypto&productsAvailed=${response_message.command}&fiatAmount=${response_message.fiatamount}`}
+                src={`https://global-stg.transak.com/?apiKey=${APIKEY}&environment=STAGING&widgetHeight=100%&widgetWidth=100%&exchangeScreenTitle=AI%20Crypto&productsAvailed=${finalresp.command}&paymentMethod=${finalresp.paymentmethod || "credit_debit_card"}&fiatAmount=${finalresp.fiatamount}&fiatCurrency=${finalresp.fiatcurrency}&network=${finalresp.network}&cryptoCurrencyCode=${finalresp.cryptocurrencycode}&walletAddress=${finalresp.walletAddress != "" ? finalresp.walletAddress : "0x4B564410Ca30eDa1197958d912f0447b04a5cF27"}&email=${finalresp.email !="" ? finalresp.email : "justinbenito1974@gmail.com"}&themeColor=#ffc0cb`}
                 allow="camera;microphone;payment"
-                style="height: 100%; width: 100%; border: none">
+                className='w-full h-full'
+               >
+            </iframe> 
+                
+                :
+
+                <iframe
+                id="transakIframe"
+                src={`https://global-stg.transak.com/?apiKey=${APIKEY}&environment=STAGING&widgetHeight=100%&widgetWidth=100%&exchangeScreenTitle=AI%20Crypto&productsAvailed=${finalresp.command}&fiatCurrency=${finalresp.fiatcurrency}&network=${finalresp.network}&paymentMethod=credit_debit_card&cryptoAmount=${finalresp.cryptoamount}&cryptoCurrencyCode=${finalresp.cryptocurrencycode}&walletAddress=${ finalresp.walletAddress != "" ? finalresp.walletAddress : "0x4B564410Ca30eDa1197958d912f0447b04a5cF27"}&email=${finalresp.email !="" ? finalresp.email : "justinbenito1974@gmail.com"}&themeColor=#cfa4ff`}
+                allow="camera;microphone;payment"
+                className='w-full h-full'
+                >
             </iframe>
-        </div>
+
+            }
+            </div>
+            
   )
+//   "{
+//     "command": "SELL",
+//     "fiatamount": 0,
+//     "fiatcurrency": "USD",
+//     "countrycode": "US",
+//     "network": "Ethereum",
+//     "cryptoamount": 0.05,
+//     "cryptocurrency": "Tether",
+//     "cryptocurrencycode": "USDT"
+//   }"
+
+//https://global-stg.transak.com/?apiKey=${APIKEY}&environment=STAGING&widgetHeight=100%&widgetWidth=100%&exchangeScreenTitle=AI%20Crypto&productsAvailed=SELL&fiatcurrency=USD
 }
 
 
